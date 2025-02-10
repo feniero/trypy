2a1173cf20f496a67293a234d2d39ce609c376be
Niero Federico <FNiero@siav.it>
2025-02-10 12:36:07 +0100

    users chose tickets

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import pandas as pd
 # Streamlit app title
 st.title("Stock Price App")
 
+# User input for ticker symbol
+tickers = st.text_input("Enter stock tickers (pointcomma-separated):", "AAPL; MSFT; GME")
+tickers = [ticker.strip().upper() for ticker in tickers.split(";") if ticker.strip()]
+
 # List of predefined stock tickers
-tickers = ["AAPL", "ENI.MI", "F", "MSFT", "T", "NIO", "EPD", "GME", "GGB", "BB"]
+#tickers = ["AAPL", "ENI.MI", "F", "MSFT", "T", "NIO", "EPD", "GME", "GGB", "BB"]
 
 # Download historical stock data
 stocks = yf.download(tickers, start="2018-01-01")["Close"]
