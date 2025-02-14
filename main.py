6d70c604de57891a82d45957d0abc6c1fe87ce0f
Niero Federico <FNiero@siav.it>
2025-02-14 15:27:44 +0100

    the new beginning_1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import yfinance as yf
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 #import from ./src
 from functions import normalize_data
