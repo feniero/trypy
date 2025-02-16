ab97b9f73fe18a3077f9e11236dc2bde83befa05
Niero Federico <FNiero@siav.it>
2025-02-16 16:10:24 +0100

    anni

--- main.py
+++ main.py
@@ -62,12 +62,13 @@ if sum(pesi) != 1:
 
 
 try:
-    dati = yf.download(tickers_validi, interval='1mo')["a Close"]
+    dati = yf.download(tickers_validi, interval='1mo')["Close"]
     dati = dati.reindex(tickers_validi, axis=1)
     dati.fillna(method="ffill", limit=1, inplace=True)
+
 except Exception as e:
-    st.error(":scream: Failed retrieve tickers data..")
-    st.write(f"Error: {e}")
+    st.error(":scream: Failed retrieve tickers data...")
+    #st.error(f"Error: {e}")
     st.stop()
 
 st.write(f"We got data from {dati.dropna().index[0].strftime("%Y-%m-%d")} and {dati.dropna().index[-1].strftime("%Y-%m-%d")} ")
@@ -86,92 +87,118 @@ try:
         hovermode="x unified"
     )
     st.plotly_chart(dn_chart, use_container_width=True)
-except ValueError:
-    st.error(":scream: Failed normalize data")
+except Exception as e:
+    st.error(":scream: Failed normalized data...")
+    #st.error(f"Error: {e}")
     st.stop()
 
 ## rolling ret
-portafogli=pd.DataFrame()
-anni=anni_input
-#return each period
-portafogli=roll_returns(portafogli,anni,dati, tickers,pesi)
+try:
+    portafogli=pd.DataFrame()
+    anni=anni_input
+    #return each period
+    portafogli=roll_returns(portafogli,anni,dati, tickers,pesi)
 
-st.subheader(f"Display portfolio :green[annualized return] on {anni} years")
-st.info('The column "annualized return over period %" are alerady a % value', icon="ℹ️")
-start_dates = portafogli.index
-end_dates = start_dates + pd.DateOffset(years=anni)
-returns = portafogli
+    st.subheader(f"Display portfolio :green[annualized return] on {anni} years")
+    st.info('The column "annualized return over period %" are alerady a % value', icon="ℹ️")
+    start_dates = portafogli.index
+    end_dates = start_dates + pd.DateOffset(years=anni)
+    returns = portafogli
 
-results = pd.DataFrame({
-    #"start date": start_dates,
-    "end period date": end_dates,
-    "annualized return over period %": returns*100
-})
-st.dataframe(results)
+    results = pd.DataFrame({
+        #"start date": start_dates,
+        "end period date": end_dates,
+        "annualized return over period %": returns*100
+    })
+    st.dataframe(results)
+
+except Exception as e:
+    st.error(":scream: Failed generate rolling returns on data...")
+    #st.error(f"Error: {e}")
+    st.stop()
 
 ## rolling ret - line chart
-rollretlinechart=px.line(portafogli.dropna(), title='rolling return')
-rollretlinechart.update_layout(
-    xaxis_title="Date",
-    yaxis_title="Return %",
-    hovermode="x unified"
-)
-rollretlinechart.add_hline(y=0, line_dash="dash", line_color="red", line_width=2)
-rollretlinechart.layout.yaxis.tickformat = ',.0%'
-st.plotly_chart(rollretlinechart, use_container_width=True)
+try:
+    rollretlinechart=px.line(portafogli.dropna(), title='rolling return')   
+    rollretlinechart.update_layout(
+        xaxis_title="Date",
+        yaxis_title="Return %",
+        hovermode="x unified"
+    )
+    rollretlinechart.add_hline(y=0, line_dash="dash", line_color="red", line_width=2)
+    rollretlinechart.layout.yaxis.tickformat = ',.0%'
+    st.plotly_chart(rollretlinechart, use_container_width=True)
 
 
 
-#rolling ret - hist chart
-hist = px.histogram(portafogli.dropna(), 
-    opacity=0.4, nbins=80, 
-    title="Histogram of Portfolio Returns"
-)
-hist.update_layout(
-    xaxis_title="Return",
-    yaxis_title="# of times",
-    hovermode="x unified"
-)
-hist.add_vline(x=0, line_dash="dash", line_color="red", line_width=2)
-hist.layout.xaxis.tickformat = ',.0%'
-st.plotly_chart(hist, use_container_width=True)
+    #rolling ret - hist chart
+    hist = px.histogram(portafogli.dropna(), 
+        opacity=0.4, nbins=80, 
+        title="Histogram of Portfolio Returns"
+    )
+    hist.update_layout(
+        xaxis_title="Return",
+        yaxis_title="# of times",
+        hovermode="x unified"
+    )
+    hist.add_vline(x=0, line_dash="dash", line_color="red", line_width=2)
+    hist.layout.xaxis.tickformat = ',.0%'
+    st.plotly_chart(hist, use_container_width=True)
 
+except Exception as e:
+    st.error(":scream: Failed create rolling charts...")
+    #st.error(f"Error: {e}")
+    st.stop()
 
 ## Portfolio Statistics
-st.write("### Portfolio Statistics")
-#st.write(portafogli.dropna().describe())
-st.write(
-    pd.DataFrame(
-        [
-            {"Data over period": "number of periods" , "value": portafogli.dropna().describe().iloc[0] },
-            {"Data over period": "mean of return", "value": (portafogli.dropna().describe().iloc[1])*100 },
-            {"Data over period": "median of return", "value": (portafogli.dropna().describe().iloc[5])*100 },
-            {"Data over period": "standard deviation", "value": (portafogli.dropna().describe().iloc[2])*100 },
-            {"Data over period": "Max return", "value": (portafogli.dropna().describe().iloc[7])*100 },
-            {"Data over period": "Min return", "value": (portafogli.dropna().describe().iloc[3])*100 }
-        ]
+try:
+    st.write("### Portfolio Statistics")
+    #st.write(portafogli.dropna().describe())
+    st.write(
+        pd.DataFrame(
+            [
+                {"Data over period": "number of periods" , "value": portafogli.dropna().describe().iloc[0] },
+                {"Data over period": "mean of return", "value": (portafogli.dropna().describe().iloc[1])*100 },
+                {"Data over period": "median of return", "value": (portafogli.dropna().describe().iloc[5])*100 },
+                {"Data over period": "standard deviation", "value": (portafogli.dropna().describe().iloc[2])*100 },
+                {"Data over period": "Max return", "value": (portafogli.dropna().describe().iloc[7])*100 },
+                {"Data over period": "Min return", "value": (portafogli.dropna().describe().iloc[3])*100 }
+            ]
+        )
     )
-)
+except Exception as e:
+    st.error(":scream: Failed create portfolio statistics...")
+    #st.error(f"Error: {e}")
+    st.stop()
 
 ## Drawdown
-st.subheader(":red[Drawdown] Statistics")
-#round(portafogli.dropna().quantile([0.0,0.1])*100,2)
-st.write(
-    pd.DataFrame(
-        [
-            {"Data over period": "max drawdown" , "value": ( round(portafogli.dropna().quantile(0.0)*100,2) ) },
-            {"Data over period": "10% quantile", "value": round(portafogli.dropna().quantile(0.1)*100,2)  },
-            {"Data over period": "mean return on 10% quantile", "value": round(portafogli.dropna()[portafogli.dropna()<=portafogli.dropna().quantile(0.1)].mean()*100,2)  },
-            {"Data over period": "5% quantile", "value": round(portafogli.dropna().quantile(0.05)*100,2)  },
-            {"Data over period": "mean return on 5% quantile", "value": round(portafogli.dropna()[portafogli.dropna()<=portafogli.dropna().quantile(0.05)].mean()*100,2)  }
-        ]
+try:
+    st.subheader(":red[Drawdown] Statistics")
+    #round(portafogli.dropna().quantile([0.0,0.1])*100,2)
+    st.write(
+        pd.DataFrame(
+            [
+                {"Data over period": "max drawdown" , "value": ( round(portafogli.dropna().quantile(0.0)*100,2) ) },
+                {"Data over period": "10% quantile", "value": round(portafogli.dropna().quantile(0.1)*100,2)  },
+                {"Data over period": "mean return on 10% quantile", "value": round(portafogli.dropna()[portafogli.dropna()<=portafogli.dropna().quantile(0.1)].mean()*100,2)  },
+                {"Data over period": "5% quantile", "value": round(portafogli.dropna().quantile(0.05)*100,2)  },
+                {"Data over period": "mean return on 5% quantile", "value": round(portafogli.dropna()[portafogli.dropna()<=portafogli.dropna().quantile(0.05)].mean()*100,2)  }
+            ]
+        )
     )
-)
-
+except Exception as e:
+    st.error(":scream: Failed calculate drawdown statistics...")
+    #st.error(f"Error: {e}")
+    st.stop()
 
 ## sharpRatio
-st.subheader("Sharp Ratio")
-rendimento_bond_a_n_anni=0.045
-st.write(
-    round(  (portafogli.dropna().mean()-rendimento_bond_a_n_anni)/portafogli.dropna().std() , 3)
-)
+try:
+    st.subheader("Sharp Ratio")
+    rendimento_bond_a_n_anni=0.045
+    st.write(
+        round(  (portafogli.dropna().mean()-rendimento_bond_a_n_anni)/portafogli.dropna().std() , 3)
+    )
+except Exception as e:
+    st.error(":scream: Failed create sharp ratio...")
+    #st.error(f"Error: {e}")
+    st.stop()
