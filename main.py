1d8c034b6c4cf47fb2b1f6d756c18b24d49a6a38
Niero Federico <FNiero@siav.it>
2025-02-16 15:44:36 +0100

    anni

--- main.py
+++ main.py
@@ -12,18 +12,7 @@ from src.functions import roll_returns
 # Streamlit interface elements
 st.title('Stock :blue[_Portfolio_] Analysis')
 
-# Input tickers, weights, years
-tickers_input = st.text_input("Enter tickers (comma separated)", "SPEA.BE, MSTR")
-tickers = [ticker.strip() for ticker in tickers_input.split(",")]
-
-weights_input = st.text_input("Enter weights (comma separated)", "40, 60")
-try:
-    pesi = [(float(x.strip())/100) for x in weights_input.split(",")]
-except ValueError:
-    st.error("Invalid weight format. Please enter numbers separated by commas.")
-    st.stop()
-
-
+## years
 anni_input = st.text_input("Enter the rolling return windows size", "8")
 try:
     if anni_input is None or anni_input == '':
@@ -38,7 +27,11 @@ except ValueError:
     st.error("Ouch! Invalid input. Please enter a valid number.")
     st.stop()
 
-# Validate tickers
+
+## Tickers
+tickers_input = st.text_input("Enter tickers (comma separated)", "SPEA.BE, MSTR")
+tickers = [ticker.strip() for ticker in tickers_input.split(",")]
+
 tickers_validi = []
 for ticker in tickers:
     info = yf.Ticker(ticker).history(period="1mo")
@@ -51,13 +44,21 @@ if not tickers_validi:
     st.error("❌ No valid tickers found. Please enter at least one valid ticker.")
     st.stop()
 
-# Validate weights sum
+
+## Weights
+weights_input = st.text_input("Enter weights (comma separated)", "40, 60")
+try:
+    pesi = [(float(x.strip())/100) for x in weights_input.split(",")]
+except ValueError:
+    st.error("Invalid weight format. Please enter numbers separated by commas.")
+    st.stop()
+
 if len(pesi) != len(tickers_validi):
     st.error("❌ Number of weights does not match the number of valid tickers. Please adjust.")
     st.stop()
 
 if sum(pesi) != 1:
-    st.error(f"❌ The sum of weights must be 100. Current sum: {( round(sum(pesi),2) )*100}. Please adjust your weights.")
+    st.error(f"❌ The sum of weights must be 100. Current sum: {( round(sum(pesi),2) )*100}... Please adjust your weights.")
     st.stop()
 
 
