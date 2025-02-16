8027bbc0b1a4c1fd9c2853d0c5f1484b343920a0
Niero Federico <FNiero@siav.it>
2025-02-16 18:20:01 +0100

    final touch

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ st.warning("*Disclaimer: This is not financial advice. All content on this appli
 st.write("This tool is useful for examining how assets behaved in different periods.")
 st.write("Here we analyze the distribution rolling returns on a given time.")
 st.write("Let's try to answer the final question: How it is gone if I bought it some time ago?")
-st.write("<br>")
+st.text("")
 # Input tickers, weights, years, and risk-free return
 try:
+    st.divider()
     st.subheader(":pushpin: Input data")
     # Tickers Input
     tickers_input = st.text_input("Enter tickers (comma separated)", "SPEA.BE, MSTR")
