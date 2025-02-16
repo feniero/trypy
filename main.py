91c9f4fb7d1a6294dce3bf1748e65f1f3e07b935
Niero Federico <FNiero@siav.it>
2025-02-16 11:06:40 +0100

    start1602

--- main.py
+++ main.py
@@ -96,6 +96,14 @@ st.plotly_chart(hist, use_container_width=True)
 st.write("### Portfolio Statistics")
 #st.write(portafogli.dropna().describe())
 st.write(portafogli.dropna().describe().iloc[0])
+st.write(
+    pd.DataFrame(
+        {
+            "number of periods": portafogli.dropna().describe().iloc[0],
+            "mean of return": portafogli.dropna().describe().iloc[1],
+        }
+    )
+)
 
 st.subheader(" max Drawdown")
 #st.write( round(portafogli.dropna().quantile([0.0])*100,2).iloc[1,1] ) 
